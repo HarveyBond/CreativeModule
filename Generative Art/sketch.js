a3c098a98a2fc15c8588d606c5add9333c4bef36
HarveyBond <harveybond1@hotmail.oc.uk>
2017-12-10 21:09:53 +0000

    'Spirograph'

--- Generative Art/sketch.js
+++ Generative Art/sketch.js
@@ -1,19 +1,16 @@
 
 let r=255; let g=255; let b= 255;
-//let diameter = 25;
 let x1; let y1;
-let w; let h;
 
 function setup() {
-  createCanvas(500,500);
+  createCanvas(594, 841);
   x1 = width/2;
   y1 = height/2;
-  w = 25;
-  h = 300;
+
   background(0);
 }
 function draw()  {
-
+  frameRate(24);
 
 if (mouseIsPressed) {
   r= random(255);
@@ -26,8 +23,9 @@ strokeWeight(2);
 stroke(r, g, b, 120);
 noFill();
 translate(x1,y1);
-for (var i = 0; i < 40; i++) {
-ellipse(0, 0, w, h);
-rotate(PI/20);
+  for (var i = 0; i < 10; i++){
+  ellipse(0, 0, mouseX - 300, mouseY - 420);
+  rotate(PI/5)
+
 }
 }
